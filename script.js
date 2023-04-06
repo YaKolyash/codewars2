cee9e4e8e59fbdb444a13b559428b55cea9c33f1
Николай <99831971+YaKolyash@users.noreply.github.com>
2023-04-06 19:34:26 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Задание:
+
+// Напишите функцию для разделения строки и преобразования ее в массив слов.
+
+
+// Test Results:
+// Basic tests
+// Testing for fixed tests
+// Completed in 2ms
+// Random tests
+// Testing for 200 random tests
+// Completed in 12ms
+// You have passed all of the tests! :)
+
+// Решение:
+
+function stringToArray(string) {
+
+    return string.trim().split(" ");
+
+}
